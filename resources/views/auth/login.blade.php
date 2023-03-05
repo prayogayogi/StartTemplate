105a95950b3fe59b3bae319728c34d0dbe29001a
prayogayogi <yogikpiter@gmail.com>
2023-03-05 19:55:54 +0700

    rubah template

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,52 +1,72 @@
 @extends("layouts.auth", ["title" => "Login"])
 @section("content")
-<div class="authincation h-100">
-    <div class="container h-100">
-        <div class="row justify-content-center h-100 align-items-center">
-            <div class="col-md-6">
-                <div class="authincation-content">
-                    <div class="row no-gutters">
-                        <div class="col-xl-12">
-                            <div class="auth-form">
-                                <h4 class="text-center mb-4">Sign in your account</h4>
-                                <form action="{{ route("login") }}" method="POST">
+<div class="section-authentication-signin d-flex align-items-center justify-content-center my-5 my-lg-0">
+    <div class="container-fluid">
+        <div class="row row-cols-1 row-cols-lg-2 row-cols-xl-3">
+            <div class="col mx-auto">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="border p-4 rounded">
+                            <div class="text-center">
+                                <h3 class="">Sign in</h3>
+                                <p>Don't have an account yet? <a href="{{ route("register") }}">Sign up here</a>
+                                </p>
+                            </div>
+                            <div class="d-grid">
+                                <a class="btn my-4 shadow-sm btn-white" href="javascript:;">
+                                <span class="d-flex justify-content-center align-items-center">
+                                    <img class="me-2" src="{{ asset('assets/assets/images/icons/search.svg') }}" width="16" alt="Image Description">
+                                    <span>Sign in with Google</span>
+                                </span>
+                                </a> <a href="javascript:;" class="btn btn-facebook"><i class="bx bxl-facebook"></i>Sign in with Facebook</a>
+                            </div>
+                            <div class="login-separater text-center mb-4"> <span>OR SIGN IN WITH EMAIL</span>
+                                <hr/>
+                            </div>
+                            <div class="form-body">
+                                <form class="row g-3" method="POST" action="{{ route("login") }}">
                                     @csrf
-                                    <div class="mb-3">
-                                        <label class="mb-1"><strong>Email</strong></label>
-                                        <input type="email" name="email" class="form-control @error("email") is-invalid @enderror" placeholder="Your Email" value="{{ old("email") }}">
-                                        @error("email")
-                                            <div class="invalid-feedback">
+                                    <div class="col-12">
+                                        <label for="inputEmailAddress" class="form-label">Email Address</label>
+                                        <input type="email" name="email" class="form-control @error("email") is-invalid @enderror" id="inputEmailAddress" placeholder="Email Address">
+                                        @error('email')
+                                            <div id="inputEmailAddress" class="invalid-feedback">
                                                 {{ $message }}
                                             </div>
                                         @enderror
                                     </div>
-                                    <div class="mb-3">
-                                        <label class="mb-1"><strong>Password</strong></label>
-                                        <input type="password" name="password" class="form-control @error("password") is-invalid @enderror" placeholder="Your Password">
-                                        @error("password")
-                                            <div class="invalid-feedback">
-                                                {{ $message }}
-                                            </div>
-                                        @enderror
-                                    </div>
-                                    <div class="row d-flex justify-content-between mt-4 mb-2">
-                                        <div class="mb-3">
-                                            <a href="/forgot-password">Forgot Password?</a>
+                                    <div class="col-12">
+                                        <label for="inputChoosePassword" class="form-label">Enter Password</label>
+                                        <div class="input-group" id="show_hide_password">
+                                            <input type="password" name="password" class="form-control border-end-0 @error('password') is-invalid @enderror" id="inputChoosePassword" placeholder="Enter Password"> <a href="javascript:;" class="input-group-text bg-transparent"><i class='bx bx-hide'></i></a>
+                                            @error('password')
+                                                <div class="invalid-feedback">
+                                                    {{ $message }}
+                                                </div>
+                                            @enderror
                                         </div>
                                     </div>
-                                    <div class="text-center">
-                                        <button type="submit" class="btn btn-primary btn-block">Sign Me In</button>
+                                    <div class="col-md-6">
+                                        <div class="form-check form-switch">
+                                            <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked>
+                                            <label class="form-check-label" for="flexSwitchCheckChecked">Remember Me</label>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6 text-end">	<a href="/forgot-password">Forgot Password ?</a>
+                                    </div>
+                                    <div class="col-12">
+                                        <div class="d-grid">
+                                            <button type="submit" class="btn btn-primary"><i class="bx bxs-lock-open"></i>Sign in</button>
+                                        </div>
                                     </div>
                                 </form>
-                                <div class="new-account mt-3">
-                                    <p>Don't have an account? <a class="text-primary" href="{{ route("register") }}">Sign up</a></p>
-                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+        <!--end row-->
     </div>
 </div>
 @endsection
---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,32 @@
+<!--*Sidebar start**-->
+<div class="dlabnav">
+    <div class="dlabnav-scroll">
+        <ul class="metismenu" id="menu">
+            <li class="mm-active">
+                <a class="mm-active" href="javascript:void()" aria-expanded="false">
+                    <i class="fas fa-home"></i>
+                    <span class="nav-text">Dashboard</span>
+                </a>
+            </li>
+
+            <li><a class="has-arrow " href="javascript:void()" aria-expanded="false">
+                    <i class="fas fa-info-circle"></i>
+                    <span class="nav-text">Setings</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="app-profile.html">Profile</a></li>
+                    <li><a href="post-details.html">Post Details</a></li>
+                    <li><a href="app-calender.html">Page</a></li>
+                    <li><a class="has-arrow" href="javascript:void()" aria-expanded="false">Manajement Menu</a>
+                        <ul aria-expanded="false">
+                            <li><a href="email-compose.html">{{ $name }}</a></li>
+                            <li><a href="email-inbox.html">Inbox</a></li>
+                            <li><a href="email-read.html">Read</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</div>
+<!--**Sidebar end**-->
